3913be9a3f93443461fe02b2469fed0bfcc3c2b4
EmilieLamps <emilie.lamps@gmail.com>
2019-12-18 16:28:54 +0100

    Fin de l'exercice

--- ex3/index.php
+++ ex3/index.php
@@ -2,7 +2,7 @@
 $page = "Exercice 3"; // Définir la variable pour changer le titre !
 $date = strftime('%A %d %B %Y');
 include '../header.php';
-setlocale(LC_ALL, 'fr_FR.UTF8');
+setlocale(LC_ALL, 'fr_FR.UTF8'); //Fonction pour passer la date en français
  ?>
  <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 3</h2>
  <h3 class="text-center pb-5">Afficher la date courante avec le jour de la semaine et le mois en toutes lettres (ex : mardi 2 août 2016)</h3>
--- ex4/index.php
+++ ex4/index.php
@@ -1,14 +1,14 @@
 <?php
-$page = "Exercice "; // Définir la variable pour changer le titre !
-
+$page = "Exercice 4"; // Définir la variable pour changer le titre !
+$dDay = time();
+$oldTimestamp = mktime(8, 2, 2016);
 include '../header.php';
  ?>
- <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice </h2>
- <h3 class="text-center pb-5"></h3>
-<?php
-
-?>
-<p></p>
+ <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 4</h2>
+ <p class="text-center pb-5">Afficher le timestamp du jour.</p>
+ <p class="text-center pb-5">Afficher le timestamp du mardi 2 août 2016 à 15h00.</p>
+<p>Le Timestamp du jour est <?= $dDay ?></p>
+<p>Le Timestamp du 2 août 2016 est <?= $oldTimestamp ?></p>
 <?php
 include '../footer.php';
 ?>
