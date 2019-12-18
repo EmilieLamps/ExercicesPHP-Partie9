f66b89e27241d24422d0c0b4ffe370d81fe710b4
EmilieLamps <emilie.lamps@gmail.com>
2019-12-18 15:55:08 +0100

    Premier envoi

---
+++ header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.4.1/solar/bootstrap.min.css" rel="stylesheet" integrity="sha384-Ya0fS7U2c07GI3XufLEwSQlqhpN0ri7w/ujyveyqoxTJ2rFHJcT6SUhwhL7GM4h9" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>PHP - Partie  9| <?= $page ?></title>
+  <!-- Définir la variable page dans chaque page -->
+</head>
+<header>
+  <h1 class="text-center text-white pb-5">Exercices PHP - Les dates</h1>
+  <!--Navbar -->
+  <nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color">
+    <a class="navbar-brand" href="#">PARTIE 9</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+    aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie9.info/ex1/index.php">Exercice 1</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie9.info/ex2/index.php">Exercice 2</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie9.info/ex3/index.php">Exercice 3</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie9.info/ex4/index.php">Exercice 4</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie9.info/ex5/index.php">Exercice 5</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie9.info/ex6/index.php">Exercice 6</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie9.info/ex7/index.php">Exercice 7</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://phppartie9.info/ex8/index.php">Exercice 8</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto nav-flex-icons">
+      <li class="nav-item">
+        <a class="nav-link waves-effect waves-light">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link waves-effect waves-light">
+          <i class="fab fa-google-plus-g"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!--/.Navbar -->
+</header>
+<body>
