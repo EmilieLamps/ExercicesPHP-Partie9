0b6df0ec33e18026f07f49d4a6dd85675e1f2230
EmilieLamps <emilie.lamps@gmail.com>
2019-12-19 10:03:55 +0100

    fin de l'exercice

--- ex5/index.php
+++ ex5/index.php
@@ -2,10 +2,9 @@
 $page = "Exercice 5"; // Définir la variable pour changer le titre !
 $date1 = '16-05-2016';
 $date2 = date('d/m/Y'); // Donne la date du jour
-// converti les dates en timestamp
+// convertir les dates en timestamp
 $timestampDate1 = strtotime($date1);
-$timestampDate2 = time();
-var_dump($timestampDate2);
+$timestampDate2 = time(); // La fonction time() permet d'afficher le timestamp du jour
 // Soustrait les variables pour obtenir la différence entre les 2 timestamp
 $soustractionDate = $timestampDate2 - $timestampDate1;
 // On divise la différence obtenue par 86400 (nombre de secondes dans une journée)
@@ -14,7 +13,7 @@ include '../header.php';
  ?>
  <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 5</h2>
  <h3 class="text-center pb-5">Afficher le nombre de jour qui sépare la date du jour avec le 16 mai 2016.</h3>
- <p><?= intval($numberDays) ?> : c'est le nombre de jour qui séparent les 2 dates</p> <!-- INTVAL permet de n'afficher que l'entier de la variable -->
+ <p><?= intval($numberDays) ?> : c'est le nombre de jours qui séparent les 2 dates</p> <!-- INTVAL permet de n'afficher que l'entier de la variable -->
 <?php
 include '../footer.php';
 ?>
--- ex6/index.php
+++ ex6/index.php
@@ -1,14 +1,11 @@
 <?php
-$page = "Exercice "; // Définir la variable pour changer le titre !
-
+$page = "Exercice 6"; // Définir la variable pour changer le titre !
+$result = cal_days_in_month(CAL_GREGORIAN, 2, 2016); // La fonction cal_days_in_month calcul le nombre de jour d'un mois. On renseigne le calendrier utilisé puis le mois et l'année.
 include '../header.php';
  ?>
- <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice </h2>
- <h3 class="text-center pb-5"></h3>
-<?php
-
-?>
-<p></p>
+ <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 6</h2>
+ <h3 class="text-center pb-5">Afficher le nombre de jour dans le mois de février de l'année 2016.</h3>
+<p>Le mois de février 2016 comptait <?= $result ?> jours.</p>
 <?php
 include '../footer.php';
 ?>
