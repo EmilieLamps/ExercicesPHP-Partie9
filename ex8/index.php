65a211fa3565e5598cce5caac2c1fc41a1adcf44
EmilieLamps <emilie.lamps@gmail.com>
2019-12-19 13:52:32 +0100

    fin de l'exercice

--- ex8/index.php
+++ ex8/index.php
@@ -1,14 +1,16 @@
 <?php
-$page = "Exercice "; // Définir la variable pour changer le titre !
-
+$page = "Exercice 8"; // Définir la variable pour changer le titre !
+$dDay = time(); // affiche la date du jour en timestamp
+$dayNumber = 22; // Modifier la variable pour modifier le calcul
+$secondsDay = $dayNumber * 86400; // converti les jours de la variable dayNumber en secondes
+$timestampResult = ($dDay - $secondsDay); // additionne le timestamp du jour + celui du nombre de jours incrémentés
+var_dump($timestampResult);
+$result = date('d/m/Y', $timestampResult); // converti le timestamp en date au format JJ/MM/AAAA
 include '../header.php';
  ?>
- <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice </h2>
+ <h2 class="text-primary text-center pt-5 pb-5 font-weight-bold">Exercice 8</h2>
  <h3 class="text-center pb-5"></h3>
-<?php
-
-?>
-<p></p>
+<p>Il y a 22 jours, nous étions le <?= $result ?></p>
 <?php
 include '../footer.php';
 ?>
